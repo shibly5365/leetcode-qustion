b676076c89b1bbce6392e432488af02ba781784c
shibly5365 <nuhm8565@gmail.com>
2026-01-03 12:35:50 +0530

    Time: 30 ms (71.33%), Space: 59.8 MB (45.7%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    let count = {};
+    for (let ch of s) {
+        count[ch] = (count[ch] || 0) + 1;
+    }
+    for (let i = 0; i < s.length; i++) {
+        if (count[s[i]] === 1) {
+            return i;
+        }
+    }
+
+    return -1;
+};
