7e3d05ec7afefce4878eb7b858ab5759a7cb0577
shibly5365 <nuhm8565@gmail.com>
2026-01-03 11:59:13 +0530

    Time: 3 ms (92.4%), Space: 76.4 MB (65.77%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    if (!head || !head.next) return true;
+
+    // Find middle
+    let slow = head;
+    let fast = head;
+
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    // Reverse second half
+    let secondHalf = reverse(slow);
+
+    // Compare halves
+    let firstHalf = head;
+    while (secondHalf) {
+        if (firstHalf.val !== secondHalf.val) {
+            return false;
+        }
+        firstHalf = firstHalf.next;
+        secondHalf = secondHalf.next;
+    }
+
+    return true;
+};
+
+function reverse(head) {
+    let prev = null;
+    while (head) {
+        let next = head.next;
+        head.next = prev;
+        prev = head;
+        head = next;
+    }
+    return prev;
+}
