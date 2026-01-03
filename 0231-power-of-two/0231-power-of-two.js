440229af765f32a2fcb027ba325af762642c1c02
shibly5365 <nuhm8565@gmail.com>
2026-01-03 11:51:03 +0530

    Time: 1 ms (30.7%), Space: 56.1 MB (35.59%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.js
+++ 0231-power-of-two/0231-power-of-two.js
@@ -3,7 +3,12 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
-if(n<=0) return false
-   return (n & (n - 1)) === 0;
+    if (n <= 0) return false;
+
+    while (n % 2 === 0) {
+        n = n / 2;
+    }
+
+    return n === 1;
     
 };
