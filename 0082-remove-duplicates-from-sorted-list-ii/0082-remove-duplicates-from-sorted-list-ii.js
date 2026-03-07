e79908b796a1411671325c6939015ffd5d4923a8
shibly5365 <nuhm8565@gmail.com>
2026-03-07 16:11:26 +0530

    Time: 1 ms (56.73%), Space: 56.2 MB (97.28%) - LeetHub

---
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+  let dummy = new ListNode(0);
+    dummy.next = head;
+
+    let prev = dummy;
+    let current = head;
+
+    while(current){
+
+        if(current.next && current.val === current.next.val){
+
+            while(current.next && current.val === current.next.val){
+                current = current.next;
+            }
+
+            prev.next = current.next;
+
+        }else{
+            prev = prev.next;
+        }
+
+        current = current.next;
+    }
+
+    return dummy.next;
+    
+};
