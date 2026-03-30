4a3a72ba80fe72f003e93a22572b858491db039a
shibly5365 <nuhm8565@gmail.com>
2026-03-30 17:17:35 +0530

    Time: 18 ms (49.92%), Space: 74 MB (51.47%) - LeetHub

---
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function(nums) {
+    let set = new Set(nums);
+    let res = [];
+
+    for (let i = 1; i <= nums.length; i++) {
+        if (!set.has(i)) {
+            res.push(i);
+        }
+    }
+
+    return res;
+}
+
