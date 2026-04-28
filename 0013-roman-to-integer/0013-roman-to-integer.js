bb8cda8ee9592bead67a20d4b5150d45456bb0f7
NUHMAN SHIBLY <nuhm8565@gmail.com>
2026-04-28 15:02:06 +0530

    Time: 112 ms (5.02%), Space: 60.6 MB (83.5%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.js
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -3,27 +3,30 @@
  * @return {number}
  */
 var romanToInt = function (s) {
-    const map = {
-        I: 1,
-        V: 5,
-        X: 10,
-        L: 50,
-        C: 100,
-        D: 500,
-        M: 1000
-    };
+     let map = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
 
-    let total = 0
-    for(let i = 0 ;i<s.length;i++){
-        let cur = map[s[i]]
-        let next  = map[s[i+1]]
+    let result = 0;
 
-        if(next>cur){
-            total -= cur
-        }else{
-            total += cur
+    for (let i = 0; i < s.length; i++) {
+        let current = map[s[i]];
+        let next = map[s[i+1]];
+
+        if (current < next) {
+            result -= current;
+        }
+        else {
+            result += current;
         }
     }
-    return total
+
+    return result;
 
 };
